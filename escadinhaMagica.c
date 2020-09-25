bb609d752fbc09af4942c65657bd307d95907647
Bruno Guimarães <correio.bfg@gmail.com>
2020-09-25 18:42:30 -0300

    correção no arquivo escadinhaMagica

--- escadinhaMagica.c
+++ escadinhaMagica.c
@@ -6,7 +6,10 @@ int main(){
     }while(n<1 || n>40);
     for (int i=1;i<=n;i++){
         for(int c=1;c<=i;c++){
-            printf("%d ",c);
+            printf("%d",c);
+            if(c<i){
+                printf(" ");
+            }
         }
         printf("\n");
     }
