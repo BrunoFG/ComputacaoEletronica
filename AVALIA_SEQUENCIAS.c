b6e9db1b1efd62c389c57b13ab5d722e88d352c7
Bruno Guimarães <correio.bfg@gmail.com>
2020-10-08 20:46:06 -0300

    .

--- AVALIA_SEQUENCIAS.c
+++ AVALIA_SEQUENCIAS.c
@@ -1,73 +1,77 @@
+// AULÃO DE REVISÃO
 #include <stdio.h>
 #include <math.h>
-
 void EXPLICA_PROGRAMA(){
-    printf("*********\n");
-    printf("Matricula:********* - F3\n");
-    printf("O programa gera duas sequências distintas a partir de quatro variáveis, calcula a distância entre todos os valores de mesmo índice, faz a somatória dos valores de cada sequência e apresenta estes dados ao usuário que terá a opção de reiniciar o código sem precisar fechar e reabrí-lo\n\n");
+    printf("nome:\n");
+    printf("matrícula: \n");
+    printf("Descrição: \n\n");
 }
-int CALCULA_TERMO(int penultimoTermo, int antepenultimoTermo){
-    return penultimoTermo + antepenultimoTermo;
+int CALCULA_TERMO(int Sn1, int Sn2){
+    return Sn1 + Sn2;
 }
-int CALCULA_DIFERENCA(int termoDeU, int termoDeT){
-    return abs(termoDeU - termoDeT);;
+int CALCULA_DIFERENCA(int Un, int Tn){
+    return abs(Un-Tn);
+}
+void IMPRIME_RESULTADOS(int indice, int Uatual, int Tatual, int somaU, int somaT){
+    int difUT = CALCULA_DIFERENCA(Uatual, Tatual);
+    int difAcumuladas = CALCULA_DIFERENCA(somaU, somaT);
+    printf("índice:%d U:%d T:%d dUT:%d somaU:%d somaT:%d dSoma:%d\n", indice, Uatual, Tatual, difUT, somaU, somaT, difAcumuladas);
 }
 int main(){
-    int U0, U1, T0, T1, k=1, dAtual, dMax=0,dLimite, repita, somaAcumuladaU=0, somaAcumuladaT=0, ultimoU, ultimoT, penultimoU, penultimoT, antepenultimoU=0, antepenultimoT=0, diferencaAcumuladas;
-    char execute;
-    EXPLICA_PROGRAMA();
-    do{
-        printf("Insira o valor que você deseja atribuir como a distância limite (mínimo: 1, máximo: 300.000)");
+    char resposta;
+    while(resposta != 'n' || resposta != 'N'){
+        int Dk, Uk, Tk, U0, U1, T0, T1, Dmax=0, k=0, Uk1, Uk2=0, Tk1, Tk2=0, Uacumulada=0, Tacumulada=0, Dlimite;
         do{    
-            scanf("%d",&dLimite);
-        }while(dLimite<1 || dLimite>300000);
+            printf("Digite a distância máxima: ");
+            scanf("%d", &Dlimite);
+        }while(Dlimite<1 || Dlimite>300000);
         do{
-            printf("Digite o primeiro termo de U: ");
-            scanf("%d",&U0);
-            printf("Digite o segundo termo de U: ");
-            scanf("%d",&U1);
-            printf("Digite o primeiro termo de T: ");
-            scanf("%d",&T0);
-            printf("Digite o segundo termo de T: ");
-            scanf("%d",&T1);
-            if(U0 == T0 && U1 == T1){repita = 1;}
-        }while(U0<0 || T0<0 || U1<=U0 || T1<=T0 || repita == 1);
-        do {
-            if(k==1){ // primeiro termo da sequência
-                penultimoU = U0;
-                penultimoT = T0;
-            }else if(k==2){ // segundo termo da sequência
-                penultimoU = U1;
-                penultimoT = T1;
-            }else if(k==3){ // terceiro termo da sequência
-                penultimoU = U1;
-                penultimoT = T1;
-                antepenultimoU = U0;
-                antepenultimoT = T0;
-            }else if (k>3){ // demais termos da sequência
-                antepenultimoU = penultimoU;
-                antepenultimoT = penultimoT;
-                penultimoU = ultimoU;
-                penultimoT = ultimoT;
+            do{
+                printf("Digite os valores iniciais de U\n");
+                scanf("%d",&U0);
+                scanf("%d",&U1);
+            }while(U0<0 || U1<=U0);
+            do{
+                printf("Digite os valores iniciais de T\n");
+                scanf("%d",&T0);
+                scanf("%d",&T1);
+            }while(T0<0 || T1<=T0);
+        }while(U0==T0 && U1==T1);
+        do{
+            if(k==0){
+                Uk1 = U0;
+                Tk1 = T0;
+            }else if(k==1){
+                Uk1 = U1;
+                Tk1 = T1;
+            }else if(k==2){
+                Uk2 = U0;
+                Tk2 = T0;
+                Uk1 = U1;
+                Tk1 = T1;
+            }else if(k>2){
+                Uk2 = Uk1;
+                Tk2 = Tk1;
+                Uk1 = Uk;
+                Tk1 = Tk;       
             }
-            ultimoU = CALCULA_TERMO(penultimoU, antepenultimoU); // calcula o termo atual da sequência U
-            ultimoT = CALCULA_TERMO(penultimoT, antepenultimoT); // calcula o termpo atual da sequência T
-            dAtual = CALCULA_DIFERENCA(ultimoU,ultimoT); // calcula a distancia entre os termos atuais de U e T
-            somaAcumuladaU += ultimoU; // acrescenta o termo atual de U ao somatório dos termos de U
-            somaAcumuladaT += ultimoT; // acrescenta o termo atual de T ao somatório dos termos de T
-            diferencaAcumuladas = CALCULA_DIFERENCA(somaAcumuladaU, somaAcumuladaT); // calcula a diferença entre as somas acumuladas
-            printf("\nA distância entre U%d:%d e T%d:%d é: %d\n",k-1,ultimoU,k-1,ultimoT,dAtual);
-            printf("As somas acumuladas de U e de T até então são %d e %d, respectivamente\n", somaAcumuladaU, somaAcumuladaT);
-            printf("A diferença entre as somas acumuladas é: %d\n", diferencaAcumuladas);
-            if (dAtual>dMax){
-                dMax = dAtual; // armazenamento da distância máxima
-            }
+            Uk = CALCULA_TERMO(Uk1, Uk2);
+            Tk = CALCULA_TERMO(Tk1, Tk2);
+            Dk = CALCULA_DIFERENCA(Uk, Tk);
+            Uacumulada += Uk;
+            Tacumulada += Tk;
+            IMPRIME_RESULTADOS(k,Uk,Tk,Uacumulada,Tacumulada);
             k++;
-        }while(dAtual<dLimite);
-        printf("A distância máxima é: %d\n\n", dMax);
-        printf("deseja rodar o código novamente? Digite 'N' ou 'n' para sair do programa\n");
-        scanf(" %c",&execute);
-        if(execute=='N' || execute=='n'){break;}
-    }while(execute!='N' || execute!='n');
-    printf("Programa finalizado!");
+            if(Dk>Dmax){
+                Dmax = Dk;
+            }
+        }while(Dk<=Dlimite);
+        printf("Deseja executar o programa novamente?\n");
+        scanf(" %c",&resposta);
+        if (resposta == 'n' || resposta=='N'){
+            break;
+        }
+    }
+    printf("Programa finalizado!\n");
+    EXPLICA_PROGRAMA();
 }
